988fb61d2349b348e7d667a74a7212df01b10be0
KL1cktm <urgzhenyok@mail.ru>
2025-02-25 17:27:45 +0300

    add button 3d mode replace remoted data to local machine

--- editorShapes/src/main/java/by/yurhilevich/editorShapes/controllers/MainPageController.java
+++ editorShapes/src/main/java/by/yurhilevich/editorShapes/controllers/MainPageController.java
@@ -15,4 +15,9 @@ public class MainPageController {
     public String lab1Page() {
         return "Editor";
     }
+
+    @GetMapping("/lab2")
+    public String lab2Page() {
+        return "Editor";
+    }
 }
